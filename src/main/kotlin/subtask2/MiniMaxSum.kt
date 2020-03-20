60a655b1c67fa35110988be0dc629749d0c95b22
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-03-21 01:41:36 +0300

    refactor: update solution for MiniMaxSum

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,13 +4,9 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        var min = Int.MAX_VALUE
-        var max = Int.MIN_VALUE
-        for (i in input.indices){
-            val resultSum = input.filterIndexed { index, _ -> index != i }.sum()
-            if(min > resultSum) min = resultSum
-            if(max < resultSum) max = resultSum
-        }
-        return IntArray(2) {if(it == 0) min else max}
+        val min = input.min()!!
+        val max = input.max()!!
+        val sum = input.sum()
+        return arrayOf(sum - max, sum - min).toIntArray()
     }
 }
