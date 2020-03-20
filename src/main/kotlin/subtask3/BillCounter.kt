71d76860ad9fcc136e7277eba041684366686c97
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-03-21 01:31:44 +0300

    feat: add solution for BillCounter task

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -5,6 +5,8 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val annaBill = bill.filterIndexed { index, _ -> index != k }.sum() / 2
+        val needToPay = b - annaBill
+        return if (needToPay != 0) needToPay.toString() else "Bon Appetit"
     }
 }
