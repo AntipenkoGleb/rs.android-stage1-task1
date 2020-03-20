84d2c85c2ab55bcdc1462783d5f488af4e406737
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-03-21 01:59:53 +0300

    refactor: update solution for HappyArray task

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,15 +4,18 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        return makeHappy(sadArray)
+        val happyArray = sadArray.toMutableList()
+        var step = 0
+        while (step != happyArray.size) {
+            if (!lessThenNearest(happyArray, step)) {
+                happyArray.removeAt(step)
+                step--
+            } else step++
+        }
+        return happyArray.toIntArray()
     }
 
-    private fun makeHappy(sadArray: IntArray): IntArray {
-        val resultArray = sadArray.filterIndexed { index, _ -> lessThenNearest(sadArray, index)  }.toIntArray()
-        return if(resultArray.size < sadArray.size ) makeHappy(resultArray) else resultArray
-    }
-
-    private fun lessThenNearest(sadArray: IntArray, index: Int): Boolean {
+    private fun lessThenNearest(sadArray: MutableList<Int>, index: Int): Boolean {
         if (index == 0 || index == sadArray.lastIndex) return true
         return sadArray[index] < (sadArray[index - 1] + sadArray[index + 1])
     }
