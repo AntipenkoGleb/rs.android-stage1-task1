03b0c95267913026a6b49c73200533b2293e044a
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-03-21 02:01:35 +0300

    refactor: update solution for BillCounter task

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -5,7 +5,7 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        val annaBill = bill.filterIndexed { index, _ -> index != k }.sum() / 2
+        val annaBill = (bill.sum() - bill[k]) / 2
         val needToPay = b - annaBill
         return if (needToPay != 0) needToPay.toString() else "Bon Appetit"
     }
