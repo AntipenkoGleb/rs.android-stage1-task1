8eb80556ecb3ebe271069f1b8f63651be2b41edf
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-03-21 01:30:28 +0300

    feat: add solution for HappyArray task

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,16 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        return makeHappy(sadArray)
+    }
+
+    private fun makeHappy(sadArray: IntArray): IntArray {
+        val resultArray = sadArray.filterIndexed { index, _ -> lessThenNearest(sadArray, index)  }.toIntArray()
+        return if(resultArray.size < sadArray.size ) makeHappy(resultArray) else resultArray
+    }
+
+    private fun lessThenNearest(sadArray: IntArray, index: Int): Boolean {
+        if (index == 0 || index == sadArray.lastIndex) return true
+        return sadArray[index] < (sadArray[index - 1] + sadArray[index + 1])
     }
 }
