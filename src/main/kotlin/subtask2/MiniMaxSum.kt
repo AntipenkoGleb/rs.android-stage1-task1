1f31b9a6dbc3c434da04f33dc38bbcbeb54301b3
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-03-21 01:49:26 +0300

    refactor: update solution for MiniMaxSum

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,8 +4,8 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        val min = input.min()!!
-        val max = input.max()!!
+        val min = input.min() ?: 0
+        val max = input.max() ?: 0
         val sum = input.sum()
         return arrayOf(sum - max, sum - min).toIntArray()
     }
