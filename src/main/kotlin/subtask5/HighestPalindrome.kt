a86d60bfd2ba6b91272bb64294e44bc5a0c2f29a
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-03-24 19:30:24 +0300

    fix: fix HighestPalindrome bugs

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -7,7 +7,7 @@ class HighestPalindrome {
         val str = digitString.toCharArray()
         if (n % 2 == 1) return "-1"
         val numberOfUnpaired = getNumberOfUnpaired(str)
-        if (numberOfUnpaired > (k - 1) && k != 1) return "-1"
+        if (numberOfUnpaired > k && k != 1) return "-1"
         val numberOfReplace = k - numberOfUnpaired
         for (i in 0 until numberOfReplace) {
             val minIndex = getMinimal(str)
@@ -23,35 +23,44 @@ class HighestPalindrome {
 
     private fun getMinimal(digitString: CharArray): Int {
         var min = Int.MAX_VALUE
+        var minIndex = 0
         for (i in digitString.indices) {
             if (digitString.isMirrorSame(i)) continue
             else {
-                if (digitString[i].toInt() < min)
-                    min = i
-                if (digitString.getMirror(i).toInt() < min)
-                    min = digitString.getMirrorIndex(i)
+                if (digitString[i].toInt() < min) {
+                    minIndex = i
+                    min = digitString[i].toInt()
+                }
+                if (digitString.getMirror(i).toInt() < min) {
+                    minIndex = digitString.getMirrorIndex(i)
+                    min = digitString[minIndex].toInt()
+                }
             }
         }
-        return min
+        return minIndex
     }
 
     private fun getMax(digitString: CharArray): Int {
         var max = Int.MIN_VALUE
+        var maxIndex = 0
         for (i in digitString.indices) {
             if (digitString.isMirrorSame(i)) continue
             else {
-                if (digitString[i].toInt() > max)
-                    max = i
-                if (digitString.getMirror(i).toInt() > max)
-                    max = digitString.getMirrorIndex(i)
+                if (digitString[i].toInt() > max) {
+                    maxIndex = i
+                    max = digitString[i].toInt()
+                }
+                if (digitString.getMirror(i).toInt() > max) {
+                    maxIndex = digitString.getMirrorIndex(i)
+                    max = digitString[maxIndex].toInt()
+                }
             }
         }
-        return max
+        return maxIndex
     }
 
     private fun CharArray.setPairValue(index: Int, value: Char) {
-        val middleElem = this.size / 2
-        val pairIndex = if (index >= middleElem) index - (middleElem - 1) else ((this.size) - 1) - index
+        val pairIndex = if (index >= (this.size / 2)) ((this.size) - (index -1)) else ((this.size) - 1) - index
         this[pairIndex] = value
         this[index] = value
     }
